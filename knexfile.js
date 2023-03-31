0bff1eefafa096314f1f3f06005ea95168cfa8bd
Allzon <Guilherme.ichiyon@gmail.com>
2023-03-30 22:11:37 -0300

    Criado .env e corrigido bug no linux

--- knexfile.js
+++ knexfile.js
@@ -1,54 +1,61 @@
+require("dotenv").config();
 const path = require("path");
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.host,
+      port: process.env.port,
+      user: process.env.user,
+      password: process.env.password,
+      database: process.env.database,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,20 +1,23 @@
 const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 
-class UserController{
-  async create(request, response){
+class UserController {
+  async create(request, response) {
     const { name, email, password } = request.body;
+    console.log("oi");
 
-    if(!name){
-      throw new AppError("O nome é obrigatório")
+    if (!name) {
+      throw new AppError("O nome é obrigatório");
     }
 
     await knex("users").insert({
-      name, email, password
-    })
+      name,
+      email,
+      password,
+    });
 
-    response.json({name, email, password})
+    response.json({ name, email, password });
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
