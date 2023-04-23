585dac0bdb3ffe67bb8ac7894442994d636030c6
JrSantiago <jrsantiaggo@gmail.com>
2023-04-23 02:56:30 -0300

    criando migration e rota de criação de usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,23 +1,43 @@
 const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
 
 class UserController {
-  async create(request, response) {
-    const { name, email, password } = request.body;
-    console.log("oi");
 
-    if (!name) {
-      throw new AppError("O nome é obrigatório");
+  async create(request, response) {
+    const { first_name, last_name, email, password, contact, cpf } = request.body;
+
+    if (![first_name, last_name, email, password, contact, cpf].every(Boolean)) {
+      throw new AppError("Preencha todos os campos");
     }
 
+    const emailExistent = await knex("users").where({ email }).first();
+
+    if (emailExistent) {
+      throw new AppError("Email já cadastrado");
+    }
+
+    const cpfExistent = await knex("users").where({ cpf }).first();
+
+    if (cpfExistent) {
+      throw new AppError("CPF já cadastrado");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+
     await knex("users").insert({
-      name,
+      first_name,
+      last_name,
       email,
-      password,
+      hashedPassword,
+      contact,
+      cpf,
     });
 
-    response.json({ name, email, password });
+    response.json({ first_name, last_name, email, contact, cpf });
   }
+  
 }
 
 module.exports = UserController;
--- src/database/knex/migrations/20230329052104_createUser.js
+++ src/database/knex/migrations/20230329052104_createUser.js
@@ -1,12 +1,15 @@
 
 exports.up = knex => knex.schema.createTable("users", table => {
   table.increments('id').primary();
-  table.string('name');
+  table.string('first_name');
+  table.string('last_name');
   table.string('email');
   table.string('password');
   table.timestamp('created_date').defaultTo(knex.fn.now());
   table.timestamp('update_date').defaultTo(knex.fn.now());
-
+  table.string('avatar');
+  table.string('contact');
+  table.string('cpf');
 })
 
 
