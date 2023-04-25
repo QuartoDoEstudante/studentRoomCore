b52bfade3963ea7e4af3db4ba76503362a90b044
JrSantiago <jrsantiaggo@gmail.com>
2023-04-25 20:47:31 -0300

    criação de token para usuário, login, e update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -34,6 +34,49 @@ class UserController {
 
     response.json({ first_name, last_name, email, contact, cpf });
   }
+
+  async update(request, response) {
+
+    const { first_name, last_name, email, password, oldPassword, contact} = request.body;
+
+    const user_id = request.user.id;
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user){
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdateEmail = await knex("users").where({ email }).first();
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user_id){
+      throw new AppError("Email já cadastrado");
+    }
+
+    user.first_name = first_name ?? user.first_name;
+    user.last_name = last_name ?? user.last_name;
+    user.email = email ?? user.email;
+    user.contact = contact ?? user.contact;
+
+    if( password && !oldPassword){
+      throw new AppError("Senha antiga não informada");
+    }
+
+    if( password && oldPassword){
+      const passwordMatch = await compare(oldPassword, user.password);
+
+      if (!passwordMatch){
+        throw new AppError("Senha antiga incorreta");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users").where({ id: user_id }).update(user);
+
+    return response.status(200).json();
+
+  }
   
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const userRoutes = require("./user.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", userRoutes)
+routes.use("/sessions", sessionsRoutes)             
 
 module.exports = routes;
