7544118ecb782ff069697e5da701a79aa1ff8cae
Junior Santiago <60077776+JrSantiaggo@users.noreply.github.com>
2023-06-03 19:52:13 -0300

    Update index.js
    Signed-off-by: Junior Santiago <60077776+JrSantiaggo@users.noreply.github.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 
 const userRoutes = require("./user.routes");
 const sessionsRoutes = require("./sessions.routes");
-const postRoutes = require("./post.routes");
 const propertyRoutes = require("./property.routes");
 
 const routes = Router();
@@ -10,7 +9,6 @@ const routes = Router();
 
 routes.use("/users", userRoutes)
 routes.use("/sessions", sessionsRoutes)             
-routes.use("/post", postRoutes)
 routes.use("/property", propertyRoutes)
 
 
